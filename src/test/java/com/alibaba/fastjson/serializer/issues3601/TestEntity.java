9c21e3c79bf7d67f19fb55b4eda280e1869a8904
Sam <11712815@mail.sustech.edu.cn>
2022-03-02 16:47:48 -0800

    TestEntity changes

--- src/test/java/com/alibaba/fastjson/serializer/issues3601/TestEntity.java
+++ src/test/java/com/alibaba/fastjson/serializer/issues3601/TestEntity.java
@@ -1,9 +1,16 @@
 package com.alibaba.fastjson.serializer.issues3601;
 
 import lombok.Data;
+import com.alibaba.fastjson.serializer.issues3601.TestEnum;
 
 @Data
 public class TestEntity {
     private TestEnum testEnum;
     private String testName;
+    public void setTestName(String name) {
+        this.testName = name;
+    }
+    public void setTestEnum(TestEnum testEnum) {
+        this.testEnum = testEnum;
+    }
 }
